9be43bc0658f9ac2132d802e973f398efa311e1e
lantuhui <shengxiaobai15@163.com>
2017-12-09 12:25:14 +0800

    init eslint configFile
    init eslint configFile

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "warn",
+            "tab"
+        ],
+        "linebreak-style": [
+            "warn",
+            "windows"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "warn",
+            "always"
+        ],
+        "no-console": ["error", {
+            "allow": ["warn","error","info"]
+        }],
+        "no-unused-vars": "warn"
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const huangxi = 'lantu';
+console.info(huangxi);
